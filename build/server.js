88f2f984d27ff56208341c44ce2271286d2a0190
chenqiaoen521 <274078815@qq.com>
2018-01-23 16:46:50 +0800

    aadd

--- build/server.js
+++ build/server.js
@@ -35,6 +35,13 @@ app.use(hotMiddleware(compiler,{
   heartbeat: 2000
 }))
 
+// force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
+  })
+})
 
 
 
