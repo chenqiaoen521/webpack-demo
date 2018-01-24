37b08720c14de4c255ff3acee5167541daca025a
chenqiaoen521 <274078815@qq.com>
2018-01-24 11:00:04 +0800

    AA

--- build/server.js
+++ build/server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const webpack = require('webpack')
 const opn = require('opn')
 
-const devMiddleware = require('webpack-dev-middleware')
-const hotMiddleware = require('webpack-hot-middleware')
 const proxyMiddleware = require('http-proxy-middleware')
 const historyApiFallback = require('connect-history-api-fallback')
 
@@ -11,29 +9,20 @@ const config = require('./webpack.base.conf.js')('development')
 const compiler = webpack(config)
 
 const proxyTable = require('./proxy')
+const devMiddleware = require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath,
+  quiet: true
+})
 
+const hotMiddleware = require('webpack-hot-middleware')(compiler, {
+  log: false,
+  heartbeat: 2000
+})
 
 const history = require('./historyfallback')
 
 const app = express()
 const port = 3000
-
-Object.keys(proxyTable).forEach(function (context) {
-  var options = proxyTable[context]
-  app.use(proxyMiddleware(context, options))
-})
-
-app.use(historyApiFallback(history))
-
-app.use(devMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-  quiet: true
-}))
-
-app.use(hotMiddleware(compiler,{
-  log: false,
-  heartbeat: 2000
-}))
 
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
@@ -43,11 +32,29 @@ compiler.plugin('compilation', function (compilation) {
   })
 })
 
+Object.keys(proxyTable).forEach(function (context) {
+  var options = proxyTable[context]
+  app.use(proxyMiddleware(context, options))
+})
 
+app.use(historyApiFallback(history))
+
+app.use(devMiddleware)
+
+app.use(hotMiddleware)
 
 app.use(express.static('./static'))
 
+const uri = 'http://localhost:' + port
+
+console.log('> Starting dev server...')
+devMiddleware.waitUntilValid(() => {
+  console.log('> Listening at ' + uri + '\n')
+  // when env is testing, don't need open it
+  opn(uri)
+})
+
 app.listen(port, function() {
   console.log('success listen to' + port)
-  opn('http://localhost:' + port)
+  // opn('http://localhost:' + port)
 })
